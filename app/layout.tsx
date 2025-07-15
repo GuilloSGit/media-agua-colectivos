1b6332c33b67787a157f22f36b0e3bc90368915d
Guillermo Andrada <guillermoandrada@gmail.com>
2025-07-14 23:40:34 -0300

    manifest

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,14 +3,24 @@ import './globals.css'
 
 export const metadata: Metadata = {
   title: 'Media Agua Colectivos',
-  description: 'Horarios de colectivos',
+  description: 'Horarios de colectivos y rutas actualizadas',
   keywords: ['Media Agua', 'Colectivos', 'Horarios', 'Transporte', 'San Juan', 'Argentina'],
   openGraph: {
     title: 'Media Agua Colectivos',
-    description: 'Horarios de colectivos',
+    description: 'Horarios de colectivos y rutas actualizadas',
     type: 'website',
     locale: 'es-AR',
     siteName: 'Media Agua Colectivos',
+    countryName: 'Argentina',
+    emails: ['admin@ga-software.dev'],
+    images: [
+      {
+        url: '/logo.png',
+        alt: 'Media Agua Colectivos',
+      },
+    ],
+    phoneNumbers: ['+54 9 387 629-5801'],
+    ttl: 4800    
   },
 }
 
@@ -21,6 +31,10 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
+      <head>
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="icon" href="/logo.png" />
+      </head>
       <body>{children}</body>
     </html>
   )
