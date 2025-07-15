00fe63436bb9e89590610bc1107d6928d148c4bf
Guillermo Andrada <guillermoandrada@gmail.com>
2025-07-14 23:04:50 -0300

    detalles de render footer

--- components/transporte/Footer.tsx
+++ components/transporte/Footer.tsx
@@ -1,17 +1,16 @@
 export default function Footer() {
-    return (
-      <footer className="bg-gray-800 text-white mt-auto">
-        <div className="container mx-auto px-4 py-6">
-          <div className="text-center">
-            <p className="text-gray-300">© 2025 Municipalidad de Media Agua, Sarmiento, San Juan</p>
-            <p className="text-gray-400 text-sm mt-2 flex flex-row gap-2 items-center justify-center">
-              <span className="font-bold">Desarrollado por 云贵正 - 2025</span>
-              <a href="mailto:admin@ga-software.dev" target="_blank" rel="noopener noreferrer">admin@ga-software.dev</a>
-              <span className="font-bold">WhatsApp:</span> <a href="https://wa.me/5493876295801" target="_blank" rel="noopener noreferrer">+54 9 387 629-5801</a>
-            </p>
+  return (
+    <footer className="bg-gray-800 text-white mt-auto">
+      <div className="container mx-auto px-4 py-6">
+        <div className="text-center">
+          <p className="text-gray-300">© 2025 Municipalidad de Media Agua, Sarmiento, San Juan</p>
+          <div className="flex flex-row flex-wrap gap-2 items-center justify-center mt-2">
+            <span className="text-xs">Desarrollado por 云贵正 - 2025</span>
+            <a className="text-xs" href="mailto:admin@ga-software.dev" target="_blank" rel="noopener noreferrer">admin@ga-software.dev</a>
+            <span className="text-xs">WhatsApp:</span> <a className="text-xs" href="https://wa.me/5493876295801" target="_blank" rel="noopener noreferrer">+54 9 387 629-5801</a>
           </div>
         </div>
-      </footer>
-    )
-  }
-  
+      </div>
+    </footer>
+  )
+}
